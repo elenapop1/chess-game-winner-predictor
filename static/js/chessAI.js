3a642413e19bf00e329ec25b4fc48e900ca7fac8
ELENA <elena0pop@gmail.com>
2024-01-02 18:00:46 +0100

    fix the display of the color to make the next move

--- static/js/chessAI.js
+++ static/js/chessAI.js
@@ -315,7 +315,9 @@ function minimax(game, depth, alpha, beta, isMaximizingPlayer, sum, color) {
 
 function checkStatus(color) {
   if (game.in_checkmate()) {
-    $('#status').html(`<b>Checkmate!</b> Oops, <b>${color}</b> lost.`);
+    // Correctly determine which color is in checkmate
+    var checkmatedColor = game.turn() === 'w' ? 'white' : 'black';
+    $('#status').html(`<b>Checkmate!</b> Oops, <b>${checkmatedColor}</b> lost.`);
   } else if (game.insufficient_material()) {
     $('#status').html(`It's a <b>draw!</b> (Insufficient Material)`);
   } else if (game.in_threefold_repetition()) {
@@ -325,7 +327,9 @@ function checkStatus(color) {
   } else if (game.in_draw()) {
     $('#status').html(`It's a <b>draw!</b> (50-move Rule)`);
   } else if (game.in_check()) {
-    $('#status').html(`Oops, <b>${color}</b> is in <b>check!</b>`);
+    // Correctly determine which color is in check
+    var inCheckColor = game.turn() === 'w' ? 'white' : 'black';
+    $('#status').html(`Oops, <b>${inCheckColor}</b> is in <b>check!</b>`);
     return false;
   } else {
     $('#status').html(`No check, checkmate, or draw.`);
